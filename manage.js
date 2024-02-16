b6dc5a006319b08d4cbcd6d369bbee1d311dfcb1
khalilovsherkhan <xalilovsherxon878@gmail.com>
2024-02-16 15:54:56 +0500

    add

---
+++ manage.js
@@ -0,0 +1,33 @@
+let buttons = 
+document.querySelector(".buttons");
+let btn = 
+document.querySelectorAll("span");
+let value = 
+document.getElementById("value");
+let toggleBtn = 
+document.querySelectorAll(".toggleBtn");
+let body = 
+document.querySelector("body");
+
+
+
+for (let i = 0; i < btn.length; i++)
+    btn[i].addEventListener("click", 
+function () {
+        if (this.innerHTML == "=") {
+            value.innerHTML = 
+eval(value.innerHTML == "") {
+    value.innerHTML = 
+eval(value.innerHTML );
+   } else {
+    if (this.innerHTML == "Clear")
+    value.innerHTML = ""
+
+    }else {
+
+    value.innerHTML +=
+this.innerHTML;
+    
+     }}
+ 
+    })
